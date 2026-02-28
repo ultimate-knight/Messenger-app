43a55a7b79392bf42c336c9c6f3d5a3bbaab7209
ultimate-knight <maaz2607@gmail.com>
2026-02-28 22:28:00 +0530

    fixed some minor issue in messages.ts

--- convex/messages.ts
+++ convex/messages.ts
@@ -4,26 +4,19 @@ import { v } from "convex/values";
 export const sendMessage = mutation({
   args: {
     conversationId: v.id("conversations"),
-    senderId:v.string(),
-    text:v.string(),
-    createdAt:v.number(),
+    senderId: v.string(),
+    text: v.string(),
+    createdAt: v.number(),
   },
   handler: async (ctx, args) => {
     await ctx.db.insert("messages", {
-        conversationId: args.conversationId,
-    senderId:args.senderId,
-    text:args.text,
-    createdAt:Date.now()
+      conversationId: args.conversationId,
+      senderId: args.senderId,
+      text: args.text,
+      createdAt: Date.now(),
     });
   },
 });
-
-// export const getMessages = query({
-//   handler: async (ctx) => {
-//     return await ctx.db.query("messages").order("desc").collect();
-//   },
-// });
-
 
 export const getMessages = query({
   args: {
@@ -32,18 +25,21 @@ export const getMessages = query({
   handler: async (ctx, args) => {
     const messages = await ctx.db
       .query("messages")
-      .filter((q) =>
-        q.eq(q.field("conversationId"), args.conversationId)
-      )
+      .filter((q) => q.eq(q.field("conversationId"), args.conversationId))
       .order("asc")
       .collect();
 
     const messagesWithSender = await Promise.all(
       messages.map(async (message) => {
-        const sender = await ctx.db.get(message.senderId as any);
+        
+        const sender = await ctx.db
+          .query("users")
+          .filter((q) => q.eq(q.field("clerkId"), message.senderId))
+          .first();
+
         return {
           ...message,
-          sender,
+          sender: sender ?? null,
         };
       })
     );
